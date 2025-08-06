8218176264f6e06a6a486ffe6c562cae2256a13a
gotti <18141824+gotti@users.noreply.github.com>
2025-08-06 17:29:31 +0900

    fix?
    Signed-off-by: gotti <18141824+gotti@users.noreply.github.com>

--- components/b_route/BRoute.h
+++ components/b_route/BRoute.h
@@ -133,8 +133,8 @@ class BRoute : public Component, public uart::UARTDevice, public libbp35::Serial
 			ESP_LOGE(TAG, "Get property encode overflow");
 			return false;
 		}
-		bp.send_sk_with_data("SKSENDTO", out_buffer.data(), len, arg::mode(1), arg::flag(false), arg::str(v6_address), arg::num16(echo::UDP_PORT),
-		                     arg::mode(2), arg::num16(len));
+		bp.send_sk_with_data("SKSENDTO", out_buffer.data(), len, arg::mode(1), arg::str(v6_address), arg::num16(echo::UDP_PORT),
+		                     arg::mode(2), arg::flag(false), arg::num16(len));
 		property_requested = millis();
 		++miss_count;
 		return true;
